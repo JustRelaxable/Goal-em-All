4711ff3125479a5eac9c8c2eda5d85910c127546
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-10-31 16:22:30 +0300

    Ball kick mechanic implemented

--- Assets/Project/Scripts/BallAnimatorController.cs
+++ Assets/Project/Scripts/BallAnimatorController.cs
@@ -7,12 +7,22 @@ public class BallAnimatorController : MonoBehaviour
     [SerializeField]
     MainCharacterInputController mainCharacterInputController;
 
+    [SerializeField]
+    Transform ballForcePosition;
+
+    public float kickPower = 1f;
+
+    private Rigidbody ballRigidbody;
+    private SphereCollider sphereCollider;
+
     private Animator animator;
     private void Awake()
     {
         mainCharacterInputController.OnTouchStart += MainCharacterInputController_OnTouchStart;
         mainCharacterInputController.OnTouchEnd += MainCharacterInputController_OnTouchEnd;
         animator = GetComponent<Animator>();
+        ballRigidbody = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>();
     }
 
     private void MainCharacterInputController_OnTouchEnd(Vector3 obj)
@@ -23,5 +33,20 @@ public class BallAnimatorController : MonoBehaviour
     private void MainCharacterInputController_OnTouchStart(Vector3 obj)
     {
         animator.SetBool("Dribbling", true);
+    }
+
+    public void OnKickAnimationEnd()
+    {
+        animator.enabled = false;
+        animator.applyRootMotion = true;
+        //transform.parent = null;
+        transform.SetParent(null, true);
+       
+        ballRigidbody.isKinematic = false;
+        sphereCollider.isTrigger = false;
+        ballRigidbody.MovePosition(ballForcePosition.position);
+
+        ballRigidbody.AddForce(ballForcePosition.transform.forward * kickPower);
+        //ballRigidbody.angularVelocity = Vector3.zero;
     }
 }
