1437420ad7cca8904988cf011e9b534cd39557ae
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-11-11 00:07:32 +0300

    Enemy running animation implemented into ai

--- Assets/Project/Scripts/EnemyAnimatorController.cs
+++ Assets/Project/Scripts/EnemyAnimatorController.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class EnemyAnimatorController : MonoBehaviour
 {
+    private EnemyCollisionController enemyCollisionController;
+    private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        enemyCollisionController = GetComponent<EnemyCollisionController>();
+        enemyCollisionController.OnPlayerTriggered += EnemyCollisionController_OnPlayerTriggered;
+    }
+
+    private void EnemyCollisionController_OnPlayerTriggered()
+    {
+        animator.SetTrigger("PlayerTriggered");
+    }
 }
