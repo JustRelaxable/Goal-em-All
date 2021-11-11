5de1096684f100a8cd46ceab289fa9a9ce15e307
JustRelaxable <31169187+JustRelaxable@users.noreply.github.com>
2021-11-12 01:19:22 +0300

    Aim Assist Implemented

--- Assets/Project/Scripts/BallAnimatorController.cs
+++ Assets/Project/Scripts/BallAnimatorController.cs
@@ -59,17 +59,64 @@ public class BallAnimatorController : MonoBehaviour
         ballRigidbody.isKinematic = false;
         ballRigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         sphereCollider.isTrigger = false;
+
+        var enemyAngleData = CheckIfEnemyIsInAngle();
+
         ballRigidbody.MovePosition(ballForcePosition.position);
-        ballRigidbody.AddForce(ballForcePosition.transform.forward * kickPower);
+        if (enemyAngleData != null)
+        {
+            var shootVector = Quaternion.AngleAxis(enemyAngleData.AngleBetween,enemyAngleData.CrossVector) * ballForcePosition.transform.forward;
+            ballRigidbody.AddForce(shootVector * kickPower);
+        }
+        else
+        {
+            ballRigidbody.AddForce(ballForcePosition.transform.forward * kickPower);
+        }
+
         //ballRigidbody.angularVelocity = Vector3.zero;
 
         UnsubscribeMainCharacterEvent();
         Destroy(gameObject, 5);
     }
 
+    private EnemyAngleData CheckIfEnemyIsInAngle()
+    {
+        var colliders = Physics.OverlapSphere(mainCharacterInputController.transform.position, 10f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            EnemyAIController enemyAIController;
+            if(colliders[i].TryGetComponent<EnemyAIController>(out enemyAIController))
+            {
+                var angle = Vector3.SignedAngle(mainCharacterInputController.transform.forward, enemyAIController.transform.position - mainCharacterInputController.transform.position,mainCharacterInputController.transform.up);
+                var crossVector = Vector3.Cross(mainCharacterInputController.transform.forward, enemyAIController.transform.position - mainCharacterInputController.transform.position);
+                if(angle >= -30 && angle <= 30)
+                {
+                    if (angle < 0)
+                        angle = -angle;
+                    return new EnemyAngleData(enemyAIController.transform,angle,crossVector);
+                }
+            }
+        }
+        return null;
+    }
+
     private void UnsubscribeMainCharacterEvent()
     {
         mainCharacterInputController.OnTouchStart-=MainCharacterInputController_OnTouchStart;
         mainCharacterInputController.OnTouchEnd -= MainCharacterInputController_OnTouchEnd;
+    }
+}
+
+public class EnemyAngleData
+{
+    public Transform EnemyTransform { get;private set; }
+    public float AngleBetween { get; private set; }
+
+    public Vector3 CrossVector { get; private set; }
+    public EnemyAngleData(Transform enemyTransform,float angleBetween,Vector3 crossVector)
+    {
+        EnemyTransform = enemyTransform;
+        AngleBetween = angleBetween;
+        CrossVector = crossVector;
     }
 }
